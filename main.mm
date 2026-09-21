0d79e6c36d14ca592c930e57d08d905256fa72da
justmearlandolol <arlandozigzag@gmail.com>
2026-09-21 22:30:36 +0700

    Initialize macOS app with borderless web view
    This code sets up a macOS application with a borderless window displaying a web view that loads 'wallpaper.html'. The application terminates when the last window is closed.

---
+++ main.mm
@@ -0,0 +1,48 @@
+#include <iostream>
+#import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+@end
+
+@implementation AppDelegate
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
+    return YES;
+}
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSApplication *app = [NSApplication sharedApplication];
+        AppDelegate *delegate = [[AppDelegate alloc] init];
+        [app setDelegate:delegate];
+        
+        NSRect screenRect = [[NSScreen mainScreen] frame];
+        
+        NSWindow *window = [[NSWindow alloc] initWithContentRect:screenRect
+                                                       styleMask:NSWindowStyleMaskBorderless
+                                                         backing:NSBackingStoreBuffered
+                                                           defer:NO];
+        
+        [window setLevel:kCGDesktopWindowLevel];
+        [window setCollectionBehavior:NSWindowCollectionBehaviorCanJoinAllSpaces | NSWindowCollectionBehaviorStationary | NSWindowCollectionBehaviorIgnoresCycle];
+        [window setIgnoresMouseEvents:YES]; 
+        
+        WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+        WKWebView *webView = [[WKWebView alloc] initWithFrame:screenRect configuration:config];
+        [webView setValue:@(NO) forKey:@"drawsBackground"];
+        
+        // KODE OTOMATIS: Membaca file 'wallpaper.html' di folder yang sama dengan aplikasi
+        NSString *exePath = [[[NSBundle mainBundle] executablePath] stringByDeletingLastPathComponent];
+        NSString *htmlPath = [exePath stringByAppendingPathComponent:@"wallpaper.html"];
+        NSURL *fileURL = [NSURL fileURLWithPath:htmlPath];
+        
+        [webView loadFileURL:fileURL allowingReadAccessToURL:[fileURL URLByDeletingLastPathComponent]];
+        
+        [window setContentView:webView];
+        [window makeKeyAndOrderFront:nil];
+        
+        [app run];
+    }
+    return 0;
+}
