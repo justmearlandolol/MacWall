4491aaaafc4297d75fac3c62d9758513d1378cc4
justmearlandolol <arlandozigzag@gmail.com>
2026-09-21 23:11:56 +0700

    Refactor main.mm for improved readability and structure

--- main.mm
+++ main.mm
@@ -1,4 +1,3 @@
-#include <iostream>
 #import <Cocoa/Cocoa.h>
 #import <WebKit/WebKit.h>
 
@@ -6,43 +5,72 @@
 @end
 
 @implementation AppDelegate
+
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
     return YES;
 }
+
 @end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+
         NSApplication *app = [NSApplication sharedApplication];
+
         AppDelegate *delegate = [[AppDelegate alloc] init];
         [app setDelegate:delegate];
-        
-        NSRect screenRect = [[NSScreen mainScreen] frame];
-        
-        NSWindow *window = [[NSWindow alloc] initWithContentRect:screenRect
-                                                       styleMask:NSWindowStyleMaskBorderless
-                                                         backing:NSBackingStoreBuffered
-                                                           defer:NO];
-        
+
+        NSScreen *screen = [NSScreen mainScreen];
+        NSRect screenRect = [screen frame];
+
+        NSWindow *window =
+            [[NSWindow alloc]
+                initWithContentRect:screenRect
+                styleMask:NSWindowStyleMaskBorderless
+                backing:NSBackingStoreBuffered
+                defer:NO];
+
         [window setLevel:kCGDesktopWindowLevel];
-        [window setCollectionBehavior:NSWindowCollectionBehaviorCanJoinAllSpaces | NSWindowCollectionBehaviorStationary | NSWindowCollectionBehaviorIgnoresCycle];
-        [window setIgnoresMouseEvents:YES]; 
-        
-        WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
-        WKWebView *webView = [[WKWebView alloc] initWithFrame:screenRect configuration:config];
+
+        [window setCollectionBehavior:
+            NSWindowCollectionBehaviorCanJoinAllSpaces |
+            NSWindowCollectionBehaviorStationary |
+            NSWindowCollectionBehaviorIgnoresCycle];
+
+        [window setIgnoresMouseEvents:YES];
+
+        WKWebViewConfiguration *config =
+            [[WKWebViewConfiguration alloc] init];
+
+        WKWebView *webView =
+            [[WKWebView alloc]
+                initWithFrame:screenRect
+                configuration:config];
+
         [webView setValue:@(NO) forKey:@"drawsBackground"];
-        
-        // KODE OTOMATIS: Membaca file 'wallpaper.html' di folder yang sama dengan aplikasi
-        NSString *exePath = [[[NSBundle mainBundle] executablePath] stringByDeletingLastPathComponent];
-        NSString *htmlPath = [exePath stringByAppendingPathComponent:@"wallpaper.html"];
-        NSURL *fileURL = [NSURL fileURLWithPath:htmlPath];
-        
-        [webView loadFileURL:fileURL allowingReadAccessToURL:[fileURL URLByDeletingLastPathComponent]];
-        
+
+        // Cari wallpaper.html di folder yang sama
+        // dengan executable.
+        NSString *exePath =
+            [[[NSBundle mainBundle] executablePath]
+                stringByDeletingLastPathComponent];
+
+        NSString *htmlPath =
+            [exePath stringByAppendingPathComponent:@"wallpaper.html"];
+
+        NSURL *fileURL =
+            [NSURL fileURLWithPath:htmlPath];
+
+        [webView loadFileURL:fileURL
+        allowingReadAccessToURL:
+            [fileURL URLByDeletingLastPathComponent]];
+
         [window setContentView:webView];
+
         [window makeKeyAndOrderFront:nil];
-        
+
         [app run];
     }
+
     return 0;
 }
